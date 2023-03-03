000ff521d35d29423b05f85ca6d1a45c99074af3
Jose Costa <jmcosta@usp.br>
2023-03-03 15:01:51 +0100

    Add slides to control params

--- prey_predator/server.py
+++ prey_predator/server.py
@@ -52,7 +52,7 @@ def wolf_sheep_portrayal(agent):
     return portrayal
 
 
-canvas_element = CanvasGrid(wolf_sheep_portrayal, 20, 20, 500, 500)
+canvas_element = CanvasGrid(wolf_sheep_portrayal, 20, 20, 600, 600)
 chart_element = ChartModule(
     [{"Label": "Wolves", "Color": "#AA0000"}, {"Label": "Sheep", "Color": "#666666"}]
 )
@@ -60,15 +60,16 @@ chart_element = ChartModule(
 model_params = {
     # ... to be completed
     "width": 20,
-    "initial_sheep": 20,
-    "initial_wolves": 50,
-    "sheep_reproduce": 0.04,
-    "wolf_reproduce": 0.04,
-    "wolf_gain_from_food": 15,
-    "grass": True,
-    "grass_regrowth_time": 30,
-    "sheep_gain_from_food": 5,
-    "aging_effect": True
+    "height": 20,
+    "grass": UserSettableParameter("checkbox", "Grass", True),
+    "aging_effect": UserSettableParameter("checkbox", "Aging_effect", True),
+    "initial_sheep": UserSettableParameter("slider", "Initial sheeps", 20, 0, 100, 1),
+    "initial_wolves": UserSettableParameter("slider", "Initial wolves", 50, 0, 100, 1),
+    "sheep_reproduce": UserSettableParameter("slider", "Sheep reproduction", 0.04, 0.01, 1, 0.01),
+    "wolf_reproduce":  UserSettableParameter("slider", "Wolf reproduction", 0.04, 0.01, 1, 0.01),
+    "wolf_gain_from_food":  UserSettableParameter("slider", "Wolf gain from food", 15, 0, 100, 1),
+    "sheep_gain_from_food": UserSettableParameter("slider", "Sheep gain from food", 5, 0, 100, 1),
+    "grass_regrowth_time": UserSettableParameter("slider", "Grass regrowth time", 30, 0, 100, 1),
 }
 
 server = ModularServer(
